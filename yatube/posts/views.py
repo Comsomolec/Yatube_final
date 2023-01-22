ab0cbf8c1c96bba7976a1627ff5f09af2f6a91ce
Сергей Сабирзянов <sssabirzyanov@yandex.ru>
2023-01-22 05:10:52 +0300

    NV7

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -39,7 +39,8 @@ def profile(request, username):
         'author': author,
         'page_obj': paginator_page(author.posts.all(), request),
         'following':
-            request.user.is_authenticated and author.following.filter(
+            request.user.is_authenticated and request.user.follower.filter(
+            author=author).exists() and author.following.filter(
             user=request.user, author=author).exists()
     })
 
@@ -131,7 +132,9 @@ def profile_follow(request, username):
 def profile_unfollow(request, username):
     get_object_or_404(
         Follow, user=request.user,
-        user__follower__author__username=username
+        # user__follower__author__username=username - тесты ЯП падают!
+        # Мои работают.
+        author=User.objects.get(username=username) 
     ).delete()
     return redirect(
         reverse('posts:profile', args=[username])
